033e0352d4a40f175b7da14a706544edf1667e13
carriegale2710 <carriegale2710@gmail.com>
2025-05-19 09:35:00 +1000

    adds reiteration todos in comments - needs to add input validation and testing

--- game/src/Game.java
+++ game/src/Game.java
@@ -4,25 +4,35 @@ import java.util.Scanner;
 
 public class Game {
 
-    public int[][] displayGrid = new int[3][3];
-    public int[][] hiddenGrid = new int[3][3];
+    public int[][] displayGrid = new int[3][3]; //what user sees
+    public int[][] hiddenGrid = new int[3][3]; //hidden info - map of mines/nums underneath
 
     public static void main(String[] args) throws Exception {
         Game game = new Game();
+
+        //setting up new game
         game.setupGrids();
         game.renderHiddenGrid();
-        game.renderDisplayGrid();
-        game.playerMove();
+        game.renderDisplayGrid(); 
+
+        //starting the game
+        game.playerMove(); // TODO: make this a loop for multiple turns
     }
 
+    //TODO - move these methods to separate classes to bvrek up code
+
+    //SECTION new game setup
     public void setupGrids() {
-        // Fill hiddenGrid with 0s
+
+        // Fill hiddenGrid with 0s - reps empty mines
         for (int[] rowArr : hiddenGrid) Arrays.fill(rowArr, 0);
 
-        // Randomly place mines
-        int mineCount = 2; // Change this for more mines
+        //generate random mine placement
+        int mineCount = 2; //add more mines
         Random rand = new Random();
         int placed = 0;
+        
+        //todo - test this, while loop might be risky
         while (placed < mineCount) {
             int row = rand.nextInt(hiddenGrid.length);
             int column = rand.nextInt(hiddenGrid[0].length);
@@ -32,7 +42,8 @@ public class Game {
             }
         }
 
-        // Calculate numbers for adjacent mines
+        //generate tile numbers (no. of adjacent mines)
+        //nested for loop for 2d array grid (array inside array)
         for (int row = 0; row < hiddenGrid.length; row++) {
             for (int column = 0; column < hiddenGrid[0].length; column++) {
                 if (hiddenGrid[row][column] == -1) continue;
@@ -53,40 +64,10 @@ public class Game {
         for (int[] rowArr : displayGrid) Arrays.fill(rowArr, -2);
     }
 
-    // player move and lose condition
-    public void playerMove() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter row (0-2): ");
-        int row = scanner.nextInt();
-        System.out.print("Enter column (0-2): ");
-        int column = scanner.nextInt();
+    //SECTION - rendering the UI //TODO - move to render package
 
-        if (hiddenGrid[row][column] == -1) {
-            displayGrid[row][column] = -1;
-            renderDisplayGrid();
-            System.out.println("boom - you stepped on a mine :(");
-        } else {
-            displayGrid[row][column] = hiddenGrid[row][column];
-            renderDisplayGrid();
-            System.out.println("safe - no mines");
-            if (checkWin()) {
-                System.out.println("Congratulations! You revealed all safe squares. You win!");
-            }
-        }
-    }
 
-    public boolean checkWin() {
-        for (int row = 0; row < hiddenGrid.length; row++) {
-            for (int column = 0; column < hiddenGrid[0].length; column++) {
-                if (hiddenGrid[row][column] != -1 && displayGrid[row][column] == -2) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    // ender the hidden grid
+    // render the hidden grid
     public void renderHiddenGrid() {
         System.out.println("Hidden Grid:");
         for (int row = 0; row < hiddenGrid.length; row++) {
@@ -100,7 +81,7 @@ public class Game {
             System.out.println();
         }
     }
-
+    
     // Render the display grid to console
     public void renderDisplayGrid() {
         System.out.println("Display Grid:");
@@ -117,5 +98,49 @@ public class Game {
             System.out.println();
         }
     }
+
+    //SECTION - game logic -> //TODO - move to play package
+
+
+
+    // player move and lose condition
+
+    public void playerMove() {
+        // TODO: add input validation - out of bounds, NaN, too many inputs etc.
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter row (0-2): ");
+        int row = scan.nextInt();
+        System.out.print("Enter column (0-2): ");
+        int column = scan.nextInt();
+        scan.close();
+        
+        //separate this logic out??
+        if (hiddenGrid[row][column] == -1) {
+            displayGrid[row][column] = -1;
+            renderDisplayGrid();
+            System.out.println("boom - you stepped on a mine :(");
+        } else {
+            displayGrid[row][column] = hiddenGrid[row][column];
+            renderDisplayGrid();
+            System.out.println("safe - no mines");
+            if (checkWin()) {
+                System.out.println("congrats you win!");
+            }
+        }
+    }
+
+    //if no non-mines remaining - user wins
+    public boolean checkWin() {
+        for (int row = 0; row < hiddenGrid.length; row++) {
+            for (int column = 0; column < hiddenGrid[0].length; column++) {
+                if (hiddenGrid[row][column] != -1 && displayGrid[row][column] == -2) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    
 
 }
